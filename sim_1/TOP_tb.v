1714905971ab6b45a7cc2bc1753ddb62204bc297
jukkanghost2 <80653155+jukkanghost2@users.noreply.github.com>
2021-09-21 16:07:11 -0300

    update top_tb

--- sim_1/TOP_tb.v
+++ sim_1/TOP_tb.v
@@ -29,11 +29,14 @@ module TOP_tb;
 	//INPUTS
   reg                       CLK;
   reg                       RESET;
-  reg   [SIZEDATA - 1:0]    SWITCHES;
+  reg    [SIZEDATA - 1:0]    SWITCHES;
   reg   [N_BUTTONS - 1:0]   BUTTONS;
   	//OUTPUTS
   wire  [SIZEDATA - 1:0]    LEDS;
   reg [SIZEOP-1:0] OPS[0:N_OPS-1];
+  reg   signed [SIZEDATA - 1:0]   DATOA;
+  reg   signed [SIZEDATA - 1:0]   DATOB;
+  reg  signed [SIZEDATA - 1:0] RESULT;
 
   
    // duration for each bit = 10 * timescale = 10 * 1 ns  = 10ns
@@ -79,16 +82,34 @@ module TOP_tb;
 		     begin
 		     #(period)
 		       SWITCHES = $random; 
+		       DATOA = SWITCHES;
 		       BUTTONS = 3'b001; //DATOA
 		       #(period*2);
 		       
-		       SWITCHES = $random; 
+		       SWITCHES = $random;
+		       if( i > 5) SWITCHES = 3; //PARA OPS SRA Y SRL
+		       DATOB = SWITCHES;
+		
 		       BUTTONS = 3'b010; //DATOB
+		       
 		       #(period*2);
-
+                
+                
 		      SWITCHES = OPS[i]; 
 		      BUTTONS = 3'b100; //OPCODE
-		       #(period);
+		       #(period*2);
+		       RESULT = LEDS;
+		       case(i)
+		          0: if((DATOA + DATOB) != RESULT) $display("%d %d %d %d error en suma", DATOA, DATOB, RESULT, DATOA + DATOB);
+		          1: if((DATOA - DATOB) != RESULT) $display("%d %d %d %d error en resta", DATOA, DATOB, RESULT, DATOA - DATOB);
+		          2: if((DATOA | DATOB) != RESULT) $display("%b %b %b %b error en or", DATOA, DATOB, RESULT, DATOA | DATOB);
+		          3: if((DATOA & DATOB) != RESULT) $display("%b %b %b %b error en and", DATOA, DATOB, RESULT, DATOA & DATOB);
+		          4: if(~(DATOA | DATOB) != RESULT) $display("%b %b %b %b error en nor", DATOA, DATOB, RESULT, ~(DATOA | DATOB));
+		          5: if((DATOA ^ DATOB) != RESULT) $display("%b %b %b %b error en xor", DATOA, DATOB, RESULT, DATOA ^ DATOB);
+		          6: if((DATOA >> DATOB) != RESULT) $display("%b %b %b %b error en srl", DATOA, DATOB, RESULT, DATOA >> DATOB);
+		          7: if((DATOA >>> DATOB) != RESULT) $display("%b %b %b %b error en sra", DATOA, DATOB, RESULT, DATOA >>> DATOB);
+		       endcase
+		       
 		      end
 		      #(period);
 
@@ -98,14 +119,5 @@ module TOP_tb;
      end
        
        
-    always @(BUTTONS)
-   begin
-                   $display("SWITCHES %d", SWITCHES);
-                   $display("BOTON %b", BUTTONS);
-                   $display("RESULT %d", LEDS);
-    end
-    
-   
-                
-    always #(period/2) CLK = ~CLK;
+     always #(period/2) CLK = ~CLK;
 endmodule
